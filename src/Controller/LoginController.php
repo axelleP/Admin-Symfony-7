635fd90ab206e9bf61bd34f491f5096394ef8503
axelleP <be.fri@hotmail.fr>
2024-11-04 20:18:11 +0100

    gestion de la connexion et déconnexion

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -6,11 +6,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Bundle\SecurityBundle\Security;
+
 class LoginController extends AbstractController
 {
-    #[Route('/', name: 'app_login')]
-    public function index(): Response
+    #[Route(path: '/', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('login/index.html.twig');
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('login/index.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error
+        ]);
+    }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(Security $security): Response
+    {
+        $security->logout(false);
+        return $this->redirectToRoute('app_index');
     }
 }
